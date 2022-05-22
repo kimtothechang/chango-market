6a4c7ee8540d546e604c6d96fec124c019a62312
chango <kimtothechang@gmail.com>
2022-05-23 00:46:45 +0900

    ✨ 주문 페이지 상태 추가

--- src/Atom.js
+++ src/Atom.js
@@ -110,3 +110,41 @@ export const totalPayment = atom({
   key: 'totalPayment',
   default: 0,
 });
+
+// 최종 주문 상품 정보
+export const finalOrderInfo = atom({
+  key: 'finalOrderInfo',
+  default: [],
+});
+
+// 최종 주문 시 주문자 정보
+export const orderUserInfo = atom({
+  key: 'orderUserInfo',
+  default: {
+    name: '',
+    phone: '',
+    phone1: '',
+    phone2: '',
+    phone3: '',
+    email: '',
+  },
+});
+
+// 최종 주문 시 배송지 정보 + 결제 수단 + 최종 동의
+export const orderShippingInfo = atom({
+  key: 'orderShippingInfo',
+  default: {
+    name: '',
+    phone: '',
+    phone1: '',
+    phone2: '',
+    phone3: '',
+    address: '',
+    zipcode: '',
+    address1: '',
+    address2: '',
+    message: '',
+    payment: '',
+    agree: false,
+  },
+});
